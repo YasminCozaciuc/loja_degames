0ffaa7dcdebb9592ffc7607054f6c5123c8ad64c
Yasmin Jannuzzi Cozaciuc <124053826+YasminCozaciuc@users.noreply.github.com>
2023-04-02 21:25:57 -0300

    Add files via upload

---
+++ loja_degames/lojadegames/src/main/java/com/lojadegames/model/Produto.java
@@ -0,0 +1,87 @@
+package com.lojadegames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produtos")
+
+public class Produto {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O nome é obrigatório")
+	@Size (min = 2, max = 100, message = "O nome deve conter no mimímo 2 e no maxímo 100 caracteres")
+	private String nome;
+	
+
+	@DecimalMin(value = "0.1", inclusive = true)
+	private Double preco;
+	
+	@NotNull(message = "Digite a quantidade")
+	private Integer quantidade;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Usuario usuario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	
+	
+
+}
